543adba541ee16a58b3476a401529ab83562955f
Your Name <your.email@example.com>
2024-05-29 17:35:00 +0300

    port 5k

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from api.v1.views import app_views
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
@@ -26,6 +26,7 @@ def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = environ.get('HBNB_API_PORT', '5000')
+    import os
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -30,3 +30,16 @@ def get_No_objects():
         "places": storage.count(Place)
     }
     return jsonify(stats)
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
